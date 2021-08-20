0db93c5e96f39186cb892d1ec7524ec8d6f43d00
Sohan Madhana <54635473+sohanmadhana@users.noreply.github.com>
2021-08-20 19:57:50 +0530

    UPDATE WSGI.PY

--- mypage/mypage/wsgi.py
+++ mypage/mypage/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mypage.mypage.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mypage.mypage.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config")
+os.environ.setdefault("DJANGO_CONFIGURATION", "Production")
 
 application = get_wsgi_application()
