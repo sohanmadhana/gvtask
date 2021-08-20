e316b8f2cbe41bece645f85c8128837650a52180
Sohan Madhana <54635473+sohanmadhana@users.noreply.github.com>
2021-08-20 20:42:17 +0530

    updated static files data

--- mypage/mypage/urls.py
+++ mypage/mypage/urls.py
@@ -3,10 +3,12 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mypageapp.urls') )
-]
+] + static(settings.STATIC_URL , document_root=settings.STATIC_ROOT)
 
